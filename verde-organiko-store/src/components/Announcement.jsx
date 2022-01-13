6566746e0baf5e79ca1acbb6f0bb2cb394c8324f
Yazan Deek <ydeek91@yahoo.com>
2022-01-13 13:19:59 -0700

    finsihed coding announcment component

---
+++ verde-organiko-store/src/components/Announcement.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+height: 30px;
+background-color:#1cac78;
+color: white;
+display:flex;
+align-items:center;
+justify-content: center; 
+font-size:14px;
+font-weight:500;`;
+
+const Announcement = () => {
+    return (
+        <Container>
+            Super Deal! Free Shipping on orders over $50
+        </Container>
+    )
+}
+
+export default Announcement
